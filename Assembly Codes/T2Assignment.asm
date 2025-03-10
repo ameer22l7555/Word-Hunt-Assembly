d30c6e8bdfae81720f01e7a49bb480b84f1e0e1c
Ameer Tufail <118817158+ameer22l7555@users.noreply.github.com>
2025-03-10 15:38:50 +0500

    Add T2Assignment.asm - Assignment implementation

---
+++ Assembly Codes/T2Assignment.asm
@@ -0,0 +1,31 @@
+[org 0x0100]
+
+jmp start
+
+	param: dw 2, 10, 0
+	mov ax,[param]
+	mov bx,[param+2]
+	mov cx, 0
+
+start:
+	cmp cx, bx ; compares cx with bx
+	je result	; if cx = bx
+	jne operation 	; if cx != bx
+	
+
+operation:
+	add ax,[param+0]
+	add cx,1
+	jmp start
+
+
+result:
+	mov [param+4],ax
+	jmp done
+
+
+
+done:
+
+	mov ax, 0x4c00
+	int 0x21
